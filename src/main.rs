5ed24a02dcc49f70be5293bcc9c40a2faef474f8
Akito-n <naki415@outlook.jp>
2025-02-04 20:24:05 +0900

    AA

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,54 @@ fn main() {
     let args = Args::parse();
 
     if let Some(project) = args.project {
-        println!("Stopping Docker Compose project: {}", project);
+        println!(
+            r#"
+██████╗  ██████╗ ███████╗██╗   ██╗██╗  ██╗ ██████╗  ██████╗ 
+██╔══██╗██╔═══██╗██╔════╝██║   ██║██║ ██╔╝██╔═══██╗  ╚██╔╝ 
+██║  ██║██║   ██║███████╗██║   ██║█████╔╝ ██║   ██║   ██║  
+██║  ██║██║   ██║╚════██║██║   ██║██╔═██╗ ██║   ██║   ██║  
+██████╔╝╚██████╔╝███████║╚██████╔╝██║  ██╗╚███ ███╔╝██████║  
+╚═════╝  ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═╝ ╚═══╝╚═╝   ╚═╝  
+
+Stopping Docker Compose project: {}
+"#,
+            project
+        );
+
         let output = Command::new("docker")
             .args(["compose", "-p", &project, "stop"])
             .output()
             .expect("Failed to execute docker compose stop");
 
         if output.status.success() {
-            println!("(╯°□°）╯︵ ┻━┻\n{} containers stopped!", project);
+            println!(
+                r#"
+╯°□°）╯︵ ┻━┻
+{} containers stopped!
+"#,
+                project
+            );
         } else {
-            eprintln!("Error stopping {}: {}", project, String::from_utf8_lossy(&output.stderr));
+            eprintln!(
+                "Error stopping {}: {}",
+                project,
+                String::from_utf8_lossy(&output.stderr)
+            );
         }
     } else {
-        println!("Stopping all running Docker containers...");
+        println!(
+            r#"
+██████╗  ██████╗ ███████╗██╗   ██╗██╗  ██╗ ██████╗  ██████╗ 
+██╔══██╗██╔═══██╗██╔════╝██║   ██║██║ ██╔╝██╔═══██╗  ╚██╔╝ 
+██║  ██║██║   ██║███████╗██║   ██║█████╔╝ ██║   ██║   ██║  
+██║  ██║██║   ██║╚════██║██║   ██║██╔═██╗ ██║   ██║   ██║  
+██████╔╝╚██████╔╝███████║╚██████╔╝██║  ██╗╚███ ███╔╝██████║  
+╚═════╝  ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═╝ ╚═══╝╚═╝   ╚═╝  
+
+Stopping all running Docker containers...
+"#
+        );
+
         let output = Command::new("sh")
             .arg("-c")
             .arg("docker stop $(docker ps -q)")
@@ -31,9 +66,17 @@ fn main() {
             .expect("Failed to execute docker stop");
 
         if output.status.success() {
-            println!("(╯°□°）╯︵ ┻━┻\nAll containers stopped!");
+            println!(
+                r#"
+╯°□°）╯︵ ┻━┻
+All containers stopped!
+"#
+            );
         } else {
-            eprintln!("Error stopping containers: {}", String::from_utf8_lossy(&output.stderr));
+            eprintln!(
+                "Error stopping containers: {}",
+                String::from_utf8_lossy(&output.stderr)
+            );
         }
     }
 }
