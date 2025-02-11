3d6866dd34e5567ff34ec88d1f5035014c946437
Akito-n <naki415@outlook.jp>
2025-02-11 13:45:53 +0900

    add mock test

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,5 +1,6 @@
 use assert_cmd::Command;
 use std::env;
+use std::process::Command as StdCommand;
 
 #[test]
 fn test_dosukoi_help() {
@@ -20,6 +21,7 @@ fn test_dosukoi_list() {
     let mut cmd = Command::cargo_bin("dosukoi").unwrap();
     cmd.arg("--ls").assert().success();
 }
+
 #[test]
 fn test_dosukoi_no_containers() {
     let mut cmd = Command::cargo_bin("dosukoi").expect("Binary not found");
@@ -27,4 +29,30 @@ fn test_dosukoi_no_containers() {
     let output = cmd.assert().success();
 
     output.stdout(predicates::str::contains("No running containers found."));
+}
+
+#[test]
+fn test_dosukoi_with_running_containers() {
+    // モックとしてdockerコンテナを作る
+    StdCommand::new("docker")
+        .args([
+            "run",
+            "-d",
+            "--name",
+            "test_container",
+            "alpine",
+            "sleep",
+            "60",
+        ])
+        .status()
+        .expect("Failed to start test container");
+
+    let mut cmd = Command::cargo_bin("dosukoi").unwrap();
+    cmd.assert()
+        .stdout(predicates::str::contains("test_container"));
+
+    StdCommand::new("docker")
+        .args(["rm", "-f", "test_container"])
+        .status()
+        .expect("Failed to remove test container");
 }
